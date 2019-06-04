4b068d52b531cc8ee00e5123c618c860fb25cc80
TrevorDHeilman <trevordheilman@gmail.com>
2019-06-04 14:52:30 -0400

    first commit

---
+++ super-potato/src/main/java/com/trevor/potato/super_potato/App.java
@@ -0,0 +1,13 @@
+package com.trevor.potato.super_potato;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
