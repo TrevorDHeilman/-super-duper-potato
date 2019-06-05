f69110ce636753227f7240fcb33c61f9f95bc963
TrevorDHeilman <trevordheilman@gmail.com>
2019-06-05 09:35:00 -0400

    Test

--- Project1/src/main/webapp/static/js/login.js
+++ Project1/src/main/webapp/static/js/login.js
@@ -29,54 +29,8 @@ window.onload = function(){
 
 
 /*
-var navbar = `
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<a class="navbar-brand">
-        <span class="badge-primary">Richard's Bookstore</span>
-    </a>
-	<ul class="navbar-nav mr-auto" id="authent">
-	</ul>
-</nav>`;
-var unauthenticated = `
-		<li class="nav-item">
-			Username: <input type="text" id="username">
-		</li>
-		<li class="nav-item">
-			Password: <input type="password" id="password">
-		</li>
-		<li class="nav-item">
-			<button class="btn btn-primary" id="login">Login</button>
-		</li>
-        `;
-var authenticated = `
-		<li class="nav-item">
-			Welcome <span id="authUserName"></span> 
-		</li>
-		<li class="nav-item">
-			<button class="btn btn-danger" id="logout">Logout</button>
-        </li>`;
-var employee=null;
-var customer=null;
-var baseURL="/BookStore/";
 
-function addNavBar(){
-	let div = document.createElement("div");
-	div.innerHTML=navbar;
-	let body = document.getElementsByTagName("body")[0];
-	body.insertBefore(div,body.childNodes[0]);
-	
-	document.getElementById("authent").innerHTML=unauthenticated;
-	
-	//add event listeners
-	document.getElementById("login").addEventListener("click",authenticate);
-	document.getElementById("password").onkeydown=checkPasswordEnter;
-	authenticate();
-}
 
-function checkPasswordEnter(){
-	if(event.which===13)
-		authenticate();
-}
 
 function authenticate() {
 	var xhttp = new XMLHttpRequest();
